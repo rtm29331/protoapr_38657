9513c659b66fbabae267885dd4111cc4b4cda66d
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-12-01 17:49:56 +0900

    戻り

--- app/controllers/protoaprs_controller.rb
+++ app/controllers/protoaprs_controller.rb
@@ -1,4 +1,6 @@
 class ProtoaprsController < ApplicationController
+  
+
   def index
     # @prototypes = Prototype.all
   end
@@ -17,4 +19,10 @@ class ProtoaprsController < ApplicationController
   # end
   
   end
+  # private
+  # def prototype_params
+  #   params.require(:protoapr).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  # end
+
+  
 end
